534eeb8347c54234864b25384911b2d856937d18
Linnea kjellqvist <lk224ac@student.lnu.se>
2026-01-20 00:12:01 +0100

    Lab 1-a: Initial commit with math functions

---
+++ 1a/main.js
@@ -0,0 +1,20 @@
+import divide, { add as addition, subtract as subtraction} from './math.js';
+import {SECRET_SAUCE} from './math.js';
+//import {PI as piValue} from './constants.js';
+
+console.log(addition (5,3));
+console.log(subtraction (10, 4));
+console.log(divide(20, 4));
+
+//console.log(piValue);
+console.log(SECRET_SAUCE);
+
+import * as MathUtils from './math.js';
+//import * as Config from './constants.js';
+
+console.log(MathUtils.add(2, 3));
+console.log(MathUtils.multiply(4,5));
+//console.log(Config.GREETING);
+
+
+
