1d46cc57d75429cdb5c2ae1af5f78b0257a7e8bd
Linnea kjellqvist <lk224ac@student.lnu.se>
2026-01-20 17:01:09 +0100

    Add rectangle math function

--- 1a/main.js
+++ 1a/main.js
@@ -1,12 +1,12 @@
 import divide, { add as addition, subtract as subtraction} from './math.js';
-import {SECRET_SAUCE} from './math.js';
+import {SECRET_SAUCE, PI} from './math.js';
 //import {PI as piValue} from './constants.js';
 
 console.log(addition (5,3));
 console.log(subtraction (10, 4));
 console.log(divide(20, 4));
 
-//console.log(piValue);
+console.log(PI);
 console.log(SECRET_SAUCE);
 
 import * as MathUtils from './math.js';
@@ -15,6 +15,13 @@ import * as MathUtils from './math.js';
 console.log(MathUtils.add(2, 3));
 console.log(MathUtils.multiply(4,5));
 //console.log(Config.GREETING);
+
+console.log("Hej");
+
+import { rectangleArea, rectanglePerimeter } from './rectangles.js';
+
+console.log("Area of 5x10 rectangle:", rectangleArea(5, 10));
+console.log("Perimeter of 5x10 recatngle:", rectanglePerimeter(5,10));
 
 
 
---
+++ 1a/rectangles.js
@@ -0,0 +1,7 @@
+export function rectangleArea(width, height){
+    return width * height;
+}
+
+export function rectanglePerimeter(width, height){
+    return 2 * (width + height);
+}
